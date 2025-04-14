d0612bfcd0e79cb55b11eb2cc8f4eac0827c814a
Scarlet Martins <scarletgam@icloud.com>
2025-04-14 13:22:45 -0400

    feat: criação de superusuario via função para testes de acesso o admin

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from rest_framework.routers import DefaultRouter
-from .views import status_view, register_view, ProcessoViewSet, ExecucaoProcessoViewSet
+from .views import status_view, register_view, ProcessoViewSet, ExecucaoProcessoViewSet, criar_usuario_teste
 
 router = DefaultRouter()
 router.register(r'processos', ProcessoViewSet)
@@ -15,6 +15,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', register_view),
+    path("criar-usuario/", criar_usuario_teste),
 ]
 
 urlpatterns += router.urls
