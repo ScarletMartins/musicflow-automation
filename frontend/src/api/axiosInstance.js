222bb585667f6bab32cd077e29edfc08851b8ea0
Scarlet Martins <scarletgam@icloud.com>
2025-04-14 15:29:40 -0400

    fix: correção .env

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,6 @@ import { useAuth } from "../context/AuthContext";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-// Hook que retorna instância do axios com interceptador de refresh
 export const useAxiosAuth = () => {
   const { accessToken, login, logout } = useAuth();
 
@@ -14,7 +13,6 @@ export const useAxiosAuth = () => {
     },
   });
 
-  // Interceptor para adicionar o token no header
   instance.interceptors.request.use((config) => {
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
@@ -22,13 +20,11 @@ export const useAxiosAuth = () => {
     return config;
   });
 
-  // Interceptor de resposta: tenta usar refresh token ao receber 401
   instance.interceptors.response.use(
     (response) => response,
     async (error) => {
       const originalRequest = error.config;
 
-      // Evita loop infinito de refresh
       if (error.response?.status === 401 && !originalRequest._retry) {
         originalRequest._retry = true;
 
@@ -42,19 +38,17 @@ export const useAxiosAuth = () => {
 
             const newAccess = res.data.access;
 
-            // Atualiza localStorage e AuthContext
             localStorage.setItem("access", newAccess);
             login(newAccess, refresh);
 
-            // Refaz a requisição original com novo token
             originalRequest.headers.Authorization = `Bearer ${newAccess}`;
             return instance(originalRequest);
           } catch (refreshError) {
-            logout(); // Refresh falhou, desloga
+            logout();
             return Promise.reject(refreshError);
           }
         } else {
-          logout(); // Não há refresh disponível
+          logout();
         }
       }
 
