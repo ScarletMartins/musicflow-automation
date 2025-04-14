3cc98bc675cb946668eca56757265ac57e7e7cd8
Scarlet Martins <scarletgam@icloud.com>
2025-04-14 11:58:19 -0400

    feat: configuração dos arquivos estáticos para produção

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Para o Render reconhecer os arquivos estáticos
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
