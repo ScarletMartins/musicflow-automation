23e5483d2abb6a68396a37c734aff5e4365d0b16
Scarlet Martins <scarletgam@icloud.com>
2025-04-15 11:04:48 -0400

    Correção login apitoken - 404

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,7 @@ export default function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`${import.meta.env.VITE_API_URL}register/`, {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/register/`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
